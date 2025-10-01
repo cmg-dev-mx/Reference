01fdd4519ce3c95c5ba2e649cd719fc3b154970b
Cesar Morales Garduno <cesar.garduno@dish.com>
2025-10-01 13:54:45 -0600

    feat: restructure project with Android and iOS folders
    - Move entire Android project to Android/ folder
    - Add new iOS project in IOS/ folder
    - Add iOS .gitignore to exclude Xcode user files
    - Maintain project structure for multi-platform development

---
+++ IOS/Reference/ReferenceTests/ReferenceTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReferenceTests.swift
+//  ReferenceTests
+//
+//  Created by Cesar Garduno on 10/1/25.
+//
+
+import Testing
+@testable import Reference
+
+struct ReferenceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
